15e1871ff92ede71df9558f33cda71391e822ee1
RakhulKumar <Rakhul.kumar007@gmail.com>
2021-09-14 13:11:07 +0530

    Update LEDControl.py

--- LEDControl.py
+++ LEDControl.py
@@ -1,7 +1,10 @@
 from tkinter import *
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
+import serial
 
+USB_PORT = "/dev/ttyACM0"
+usb = serial.Serial(USB_PORT, 9600, timeout=2)
 
 window= Tk()
 window.geometry("1000x600")
@@ -10,17 +13,21 @@ window.configure(bg="white")
 text = Text(window)
 text.insert(INSERT,"LED Control Using RaspberryPi")
 
-
+'''
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(21,GPIO.OUT)
-
+'''
 
 def on():
-    GPIO.output(21, True)
+    #GPIO.output(21, True)
+    usb.write(b'led_on')
+    print("Arduino LED turned on.")
 
 def off():
-    GPIO.output(21, False)
+    #GPIO.output(21, False)
+    usb.write(b'led_off')
+    print("Arduino LED turned off.")
 
 
 
