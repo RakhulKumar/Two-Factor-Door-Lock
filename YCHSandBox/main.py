4bbb5628ef54d106a7685fb5f67fc4a259c5f2d7
Rakhul Kumar <Rakhul.Kumar007@gmail.com>
2021-09-19 17:44:06 +0530

    Update main.py

--- YCHSandBox/main.py
+++ YCHSandBox/main.py
@@ -4,10 +4,11 @@ import os
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtCore import QObject, Slot, Signal
+from PySide2 import QtGui, QtCore, QtWidgets
 
 import serial
 
-os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
+
 
 USB_PORT = "/dev/ttyACM0"
 usb = serial.Serial(USB_PORT, 9600, timeout=2)
@@ -35,7 +36,22 @@ class MainWindow(QObject):
         else:
             off()
 
+def handleVisibleChanged():
+    if not QtGui.QGuiApplication.inputMethod().isVisible():
+        return
+    for w in QtGui.QGuiApplication.allWindows():
+        if w.metaObject().className() == "QtVirtualKeyboard::InputView":
+            keyboard = w.findChild(QtCore.QObject, "keyboard")
+            if keyboard is not None:
+                r = w.geometry()
+                r.moveTop(keyboard.property("y"))
+                w.setMask(QtGui.QRegion(r))
+                return
+
+
+
 if __name__ == "__main__":
+    os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
