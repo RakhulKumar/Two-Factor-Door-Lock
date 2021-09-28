d9e208a5f9b5df78e07b22993ca6375b4315eeb0
Rakhul Kumar <Rakhul.Kumar007@gmail.com>
2021-09-28 15:52:03 +0530

    Update ImgCap.py

--- YCHSandBox/ImgCap.py
+++ YCHSandBox/ImgCap.py
@@ -1,12 +1,26 @@
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('videos/wa.avi')
-while(cap.isOpened()):
-  ret, frame = cap.read()
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  cv2.imshow('frame',gray)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+import time
 
-cap.release()
-cv2.destroyAllWindows()
+def securityCam():
+  cap = cv2.VideoCapture(0)
+  time.sleep(0.5)
+  ret, frame = cap.read()
+  
+  #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  #gray = cv2.resize(gray,(600,600))
+    #cv2.imshow('frame',gray)
+  cv2.waitKey(0)
+  cv2.imwrite('frame.png',frame)
+    
+    #if(cv2.waitKey(1) & 0xFF == ord('q')):
+      
+    #  break
+
+  cap.release()
+  cv2.destroyAllWindows()
+
+
+
+securityCam()
+
